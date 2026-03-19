169183451d033f23c6f977f83b6ff7ffbeaa6145
Sophie-estrada <estradasophie1018@gmail.com>
2026-03-19 02:18:20 +0000

    here

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
@@ -8,26 +8,27 @@ directory ) and moves to the corners of the screen in a square pattern.
 import turtle
 tina = turtle.Turtle()
 
+def set_turtle_image(turtle, image_name):
+   
+    from pathlib import Path
+    image_dir = Path(__file__).parent.parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+
+set_turtle_image(tina, 'pikachu.gif')
+
 tina.penup()
 tina.speed(3)
 
 for i in range(4):
     tina.goto(200, 200)
     tina.goto(-200, -200)
-
-def set_turtle_image(turtle, image_name):
-   
-    from pathlib import Path
-    image_dir = Path(__file__).parent / "images"
-    image_path = str(image_dir / image_name)
-
-    screen = turtle.getscreen()
-    screen.addshape(image_path)
-    turtle.shape(image_path)
-    set_turtle_image(tina, "pikachu.gif")
-
-
-screen = turtle.Screen()
-screen.setup(width=600, height=600)
 
 turtle.exitonclick()
