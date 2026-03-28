01c077d5e30907332af59f2fb5de6ba691b91b61
Sophie-estrada <estradasophie1018@gmail.com>
2026-03-28 18:07:32 +0000

    New projects

--- lessons/10_Turtles/60_More_Turtle_Programs/30_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/30_More_Turtle_Programs.py
@@ -6,12 +6,14 @@ Then change the code so that the turtle has a different image ( look in the 'ima
 directory ) and moves to the corners of the screen in a square pattern. 
 """
 import turtle
+from PIL import Image
+from pathlib import Path
+
 
 def set_background_image(window, image_name):
     """Set the background image of the turtle window to the image with the given name."""
 
-    from pathlib import Path
-    from PIL import Image
+    
 
     image_dir = Path(__file__).parent.parent / "images"
     image_path = str(image_dir / image_name)
--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -1,13 +1,36 @@
-"""
-Copy the code from the previous lesson, 08a_More_Turtle_programs.ipynb, 
-from the section " Click on the Turtle"
+import random
+x = random.randint(-300, 300)
+y = random.randint(-300, 300)
 
-Then change the code so that the turtle has a different image ( look in the 'images'
-directory ) and when you click on it, it moves to a random location on the screen.
+import turtle as turtle
 
-Use this code to get a random x and y location
+turtle.setup(width=600, height=600)
 
-    import random
-    x = random.randint(-300, 300)
-    y = random.randint(-300, 300)
-"""
+t = turtle.Turtle()
+
+t.shape("turtle")
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4) # Make the turtle really big
+
+def turtle_clicked(t, x, y):
+    """Function that gets called when the user clicks on the turtle
+
+    This function will make the turtle tilt 20 degrees 18 times, making a full
+    circle. It is called by the turtle when the user clicks on it.
+
+    Args:
+        t (Turtle): The turtle object that was clicked
+        x (int): The x coordinate of the click
+        y (int): The y coordinate of the click
+    """
+
+    print('turtle clicked!')
+    
+    for i in range(0,360, 20): # Full circle, 20 degrees at a time
+        t.tilt(20) # Tilt the turtle 20 degrees
+
+# Connect the turtle to the turtle_clicked function
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
+
+    
